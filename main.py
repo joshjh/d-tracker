c1487c4f07dfd7f0d5f4b359e29f5a8df8769432
Josh Harney <joshjames.harney@googlemail.com>
2014-02-08 21:18:53 +0100

    handles multi devices on the same external IP.  Exception handling improved to stay alive.  Now works with ip addr returning either wlo1 or wlan0.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from time import sleep
 import markup
 import re
 
-GLO_SERVER_ADD = 'joshjh.dyndns-home.com'
+GLO_SERVER_ADD = 'beautybysharon.co.uk'
 GLO_SERVER_PORT = 21954 
 PAGEFILE = '/var/www/dt/index.html'
 
@@ -19,6 +19,8 @@ class d_tracker_client(object):
     def __init__(self, send_handler):
         # these send calls are a little ugly, might be work unpacking a tupple for parameters
         print('attemping to send first update')
+        print("{} at: ('{}')('{}')".format(self.__checkin__(), self.__gettime__(), socket.gethostname()),
+                                  GLO_SERVER_ADD, GLO_SERVER_PORT)
         send_handler.send("{} at: ('{}')('{}')".format(self.__checkin__(), self.__gettime__(), socket.gethostname()),
                                   GLO_SERVER_ADD, GLO_SERVER_PORT)
         print('sent first update')
@@ -38,14 +40,23 @@ class d_tracker_client(object):
 
     def __checkin__(self):
         """ gets the external ip from a website and local ip, returns it as string in a tupple"""
-        print('getting IP info')
-        http = urllib3.PoolManager()
-        r = http.request('GET', 'http://ipecho.net/plain')
-        intf = 'wlan0'
-        intf_ip = subprocess.check_output("/bin/ip address show dev " + intf, shell=True).decode().split()
-        intf_ip = intf_ip[intf_ip.index('inet') + 1].split('/')[0]
-        print('got IP info')
-        return (r.data.decode(), intf_ip)
+        try:
+            # subprocess may through CalledProcessError if it fails
+            print('getting IP info')
+            http = urllib3.PoolManager()
+            r = http.request('GET', 'http://ipecho.net/plain')
+            if 'wlo1' in subprocess.check_output("/bin/ip address", shell=True).decode():
+                intf = 'wlo1'
+            else:
+                intf = 'wlan0'
+
+            intf_ip = subprocess.check_output("/bin/ip address show dev " + intf, shell=True).decode().split()
+            intf_ip = intf_ip[intf_ip.index('inet') + 1].split('/')[0]
+            print('got IP info')
+            return (r.data.decode(), intf_ip)
+        except (subprocess.CalledProcessError, urllib3.exceptions.HTTPError) as e:
+            print ('Failed to collect IP info, Caught ',e)
+            print('continuing...')
 
 class d_tracker_server(object):
 
@@ -56,14 +67,21 @@ class d_tracker_server(object):
     def message_handle(self, msg):
         x = re.findall("('\d+.*?\d')", msg)
         y = re.findall("('\w+')", msg)
-        if str(x[0]) in str(self.rpis):
-            print(x, 'known in rpis')
+        # need IndexError exception
+        try:
+            #lists are nested so we need to iter over the lists in self.rpis
+            for dev in self.rpis:
+                if (x[0]) and (x[1]) in dev:
+                    print(x, 'known in rpis')
 
-        else:
-            self.rpis.append(x + y)
-            print('appended:', x)
-            print (self.rpis)
-            self.__genpage__()
+            else:
+                self.rpis.append(x + y)
+                #print('appended:', x)
+                #print (self.rpis)
+                self.__genpage__()
+
+        except IndexError as e:
+            print('unable to slice recieved data to append')
 
     def __genpage__(self):
         title = "Josh's RPI devices"
@@ -87,14 +105,19 @@ class hcs_socket_listen(object):
         s.bind(('', GLO_SERVER_PORT))
         s.listen(5)
 
-        while True:
-            #  s.accept returns client (cli) object, and connected address (addr)
-            cli, addr = s.accept()
-            print ('hcs-socket got connect from ', addr)
-            cli.send(b'READY:')
-            msg = bytes.decode(cli.recv(1024))  # recieve 1024
-            dt.message_handle(msg)
-        s.close()
+        try:
+            while True:
+                #  s.accept returns client (cli) object, and connected address (addr)
+                cli, addr = s.accept()
+                print ('hcs-socket got connect from ', addr)
+                cli.send(b'READY:')
+                msg = bytes.decode(cli.recv(1024))  # recieve 1024
+                dt.message_handle(msg)
+            s.close()
+        except KeyboardInterrupt:
+            s.close()
+        finally:
+             s.close()
 
 class hcs_socket_send(object):
 
@@ -104,16 +127,19 @@ class hcs_socket_send(object):
     def send(self, msg, t_host, t_port):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind(('', 0))
-        self.s.connect((t_host, t_port))
-        s_handshake = bytes.decode(self.s.recv(1024))
+        try:
+            self.s.connect((t_host, t_port))
+            s_handshake = bytes.decode(self.s.recv(1024))
 
-        if s_handshake == 'READY:': # master awaits data
-            self.s.send(bytes(msg, 'ascii'))
-            self.s.close()
-        else:
-            print ('unable to send, bad response from master')
-            self.s.close()
-
+            if s_handshake == 'READY:': # master awaits data
+                self.s.send(bytes(msg, 'ascii'))
+                self.s.close()
+            else:
+                print ('unable to send, bad response from master')
+                self.s.close()
+        except OSError as e:
+            print('caught OSError: ',e)
+            print('continuing...')
 
 def main(client=False, server=False):
 
